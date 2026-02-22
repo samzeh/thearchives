405f528ce2a07029785830057cca9ac20362b469
Samuel <124836035+samzeh@users.noreply.github.com>
2026-02-22 00:31:36 -0500

    create get book information function

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
 def cleanData(df, columns_to_keep, *args):
   clean_books_df = df[columns_to_keep].copy(deep=True)
@@ -9,3 +10,17 @@ def cleanData(df, columns_to_keep, *args):
 def build_vectors(clean_books_df):
   vectorizer = TfidfVectorizer()
   result = vectorizer.fit_transform(clean_books_df['group_text'].tolist())
+
+def getBookDetails(book_id):
+    books_df = pd.read_csv('./data/books.csv')
+
+    row = books_df[books_df['book_id'] == book_id]
+    authors = row['authors'].values[0]
+    average_rating = row['average_rating'].values[0]
+    description = row['description'].values[0]
+    genres = row['genres'].values[0]
+    image_url = row['image_url'].values[0]
+    isbn13 = row['isbn13'].values[0]
+    pages = row['pages'].values[0]
+    
+    return authors, average_rating, description, genres, image_url, isbn13, pages
