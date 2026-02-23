61002ba7665d60e8df6f9f47713fc6f7f7c79db7
Samuel <124836035+samzeh@users.noreply.github.com>
2026-02-23 17:47:46 -0500

    create recommendation graph model v1

--- app.py
+++ app.py
@@ -22,20 +22,53 @@ def bookRecommendation(liked_book_id, top_k=5):
 
     top_indices = similarities.argsort()[-top_k:][::-1]
     recommended_books = []
+
+    books_df = pd.read_csv('./data/raw/books.csv')
   
     for i in top_indices:
-        recommended_books.append(book_id_list[i])
+        row = books_df[books_df['book_id'] == i+1]
+        authors = row['authors'].values[0]
+        average_rating = row['average_rating'].values[0]
+        description = row['description'].values[0]
+        genres = row['genres'].values[0]
+        image_url = row['image_url'].values[0]
+        isbn13 = row['isbn13'].values[0]
+        pages = row['pages'].values[0]
+        title = row['title'].values[0]
+
+        recommended_books.append({
+            "title": title,
+            "book_id": book_id_list[i],
+            "authors": authors,
+            "average_rating": average_rating,
+            "description": description,
+            "genres": genres,
+            "image_url": image_url,
+            "isbn13": isbn13,
+            "pages": pages
+        })
 
     print(f"Recommended book IDs for book {liked_book_id}: {recommended_books}")
     return recommended_books
 
+# recommended_books=bookRecommendation(68)
+
+# print("Recommended id:")
+# for book in recommended_books:
+#     print("-", book["book_id"])
+
+# print("Recommended titles:")
+# for book in recommended_books:
+#     print("-", book["title"])
+
+
+
+
 
 liked_book_id = 68  # "Perks of Being a Wallflower"
-bookRecommendation(liked_book_id)
-
-
+# bookRecommendation(liked_book_id)
 def getBookDetails(book_id):
-    books_df = pd.read_csv('./data/books.csv')
+    books_df = pd.read_csv('./data/raw/books.csv')
 
     row = books_df[books_df['book_id'] == book_id]
     authors = row['authors'].values[0]
@@ -45,5 +78,38 @@ def getBookDetails(book_id):
     image_url = row['image_url'].values[0]
     isbn13 = row['isbn13'].values[0]
     pages = row['pages'].values[0]
-    
-    return authors, average_rating, description, genres, image_url, isbn13, pages
+    title = row['title'].values[0]
+    return title, authors, average_rating, description, genres, image_url, isbn13, pages
+
+
+def createRecommendationGraph(liked_book_id, depth=3, top_k = 5, nodes=None, links=None, already_recommended=None):
+    if nodes is None:
+        nodes = []
+    if links is None:
+        links = []
+    if already_recommended is None:
+        already_recommended = set()
+
+    if liked_book_id not in already_recommended:
+        nodes.append({"id": 0, "book_id": liked_book_id, "label": getBookDetails(liked_book_id)[0], "authors": getBookDetails(liked_book_id)[1], "average_rating": getBookDetails(liked_book_id)[2], "description": getBookDetails(liked_book_id)[3], "genres": getBookDetails(liked_book_id)[4], "image_url": getBookDetails(liked_book_id)[5], "isbn13": getBookDetails(liked_book_id)[6], "pages": getBookDetails(liked_book_id)[7]})
+        already_recommended.add(liked_book_id)
+
+    recommended_books = bookRecommendation(liked_book_id, top_k)
+
+    for i, book in enumerate(recommended_books):
+        links.append({"source": liked_book_id, "target": book["book_id"]})
+
+        if book["book_id"] not in already_recommended:
+            nodes.append({"id": i+1, "book_id": book["book_id"], "label": book["title"], "authors": book["authors"], "average_rating": book["average_rating"], "description": book["description"], "genres": book["genres"], "image_url": book["image_url"], "isbn13": book["isbn13"], "pages": book["pages"]})
+
+        if depth > 1 and book["book_id"] not in already_recommended:
+            createRecommendationGraph(book["book_id"], depth - 1, top_k, nodes, links, already_recommended)
+        already_recommended.add(book["book_id"])
+
+    return nodes, links
+
+        
+nodes, links = createRecommendationGraph(68)
+print("Nodes:", nodes)
+print("Links:", links)
+
